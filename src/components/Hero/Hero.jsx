69d62b887cd838b580d8dca236bb92b5a4c19873
Tae Hoon Kim <taehoonkim@Tae-Hoons-MacBook-Pro.local>
2025-11-03 12:07:46 -0500

    test cloudflare

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,24 @@ import lgm_easter from "@/assets/lgm_easter.jpg";
 
 export default function Hero() {
   return (
-    <section
-      style={{
-        position: "fixed",
-        inset: 0, // top:0 right:0 bottom:0 left:0
-        backgroundImage: `url(${lgm_easter})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    />
+    <main>
+      <section
+        style={{
+          position: "fixed",
+          inset: 0, // top:0 right:0 bottom:0 left:0
+          backgroundImage: `url(${lgm_easter})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div>
+          <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
+            He is Risen
+          </h1>
+          <p style={{ fontSize: "1.25rem" }}>Living Grace Ministry</p>
+        </div>
+      </section>
+    </main>
   );
 }
