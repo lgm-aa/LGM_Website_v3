fa9afc67deb86259841fef9f5f6ab0e215006db9
Tae Hoon Kim <taehoonkim@Tae-Hoons-MacBook-Pro.local>
2025-11-17 14:58:16 -0500

    ../.. -> @

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Home.css";
-import NavBar from "../../components/layout/NavBar/NavBar";
-import Hero from "../../components/home/Hero/Hero";
-import Community from "../../components/home/Community/Community";
-import Welcome from "../../components/home/Welcome/Welcome";
-import Plan from "../../components/home/Plan/Plan";
-import Updates from "../../components/home/Updates/Updates";
-import Ministries from "../../components/home/Ministries/Ministries";
-import Footer from "../../components/layout/Footer/Footer";
+import NavBar from "@/components/layout/NavBar/NavBar";
+import Hero from "@/components/home/Hero/Hero";
+import Community from "@/components/home/Community/Community";
+import Welcome from "@/components/home/Welcome/Welcome";
+import Plan from "@/components/home/Plan/Plan";
+import Updates from "@/components/home/Updates/Updates";
+import Ministries from "@/components/home/Ministries/Ministries";
+import Footer from "@/components/layout/Footer/Footer";
 
 export default function Home() {
   return (
