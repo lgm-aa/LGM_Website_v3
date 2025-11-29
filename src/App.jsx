a96cabc0e536f149e55f05942b6f18288635e8c6
Tae Hoon Kim <taehoonkim@Taes-Mac-Studio.local>
2025-11-28 20:51:30 -0500

    fixed casing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/layout/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import About from "./components/about/About";
-import Sermons from "./components/sermons/sermons";
+import Sermons from "./components/sermons/Sermons";
 
 function App() {
   return (
