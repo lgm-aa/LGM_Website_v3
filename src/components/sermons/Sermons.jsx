ab986c8db54e7cc73791375a591ab9389ba51400
Tae Hoon Kim <taehoonkim@Taes-Mac-Studio.local>
2025-11-28 20:48:11 -0500

    sermons page + branch check

---
+++ src/components/sermons/Sermons.jsx
@@ -0,0 +1,11 @@
+import "./Sermons.css";
+
+export default function Sermons() {
+    return (
+        <div className="sermons-section">
+            <h1>SERMONS</h1>
+        </div>
+        
+
+    )
+}
