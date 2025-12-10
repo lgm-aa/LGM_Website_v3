41a8790b4935606d10d58179488c4017fc93891c
Joyce Ku <joyceku1020@gmail.com>
2025-12-10 17:58:03 -0500

    add footer

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/about/Hero";
 import Quote from "@/components/about/Quote";
+import Footer from "@/components/layout/Footer/Footer";
 
 export default function About() {
   return (
     <>
       <Hero />
       <Quote />
+      <Footer />
     </>
   );
 }
