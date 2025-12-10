470de7a4421a5ad788f5d119873d580cd7a27969
Joyce Ku <joyceku1020@gmail.com>
2025-12-10 17:46:42 -0500

    added quote

--- src/components/about/Quote.jsx
+++ src/components/about/Quote.jsx
@@ -3,7 +3,9 @@ import "./Quote.css";
 export default function Quote() {
     return (
         <div className="quote-container">
-            <h1 className="quote-text">The purpose of Living Grace Ministry is to nurture genuine Christian discipleship through grace.</h1>
+            <h1 className="quote-text">The purpose of Living Grace Ministry <br />
+            is to nurture <span className="highlight-text">genuine</span> Christian <br />
+            discipleship through <i>grace</i>.</h1>
         </div>
         
 
