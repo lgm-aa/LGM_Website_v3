930fe58f5f29bf25557e139a3ad8229d0efba440
Joyce Ku <joyceku1020@gmail.com>
2026-01-07 14:15:53 -0500

    added quotes on about page

--- src/components/about/Quote.jsx
+++ src/components/about/Quote.jsx
@@ -1,13 +1,18 @@
 import "./Quote.css";
+import quoteLeft from '@/assets/left_quote.svg';
+import quoteRight from '@/assets/right_quote.svg';
 
 export default function Quote() {
     return (
         <div className="quote-container">
-            <h1 className="quote-text">The purpose of Living Grace Ministry <br />
-            is to nurture <span className="highlight-text">genuine</span> Christian <br />
-            discipleship through <i>grace</i>.</h1>
+            <h1 className="quote-text">
+                <img src={quoteLeft} alt="left" className="quote-left" />
+                The purpose of Living Grace Ministry <br />
+                is to nurture <span className="highlight-text">genuine</span> Christian <br />
+                discipleship through <i>grace</i>.
+                <img src={quoteRight} alt="left" className="quote-right" />
+            </h1>
+            <hr className="quote-line"/>
         </div>
-        
-
     )
 }
