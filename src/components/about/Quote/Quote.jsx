6e042a2a6a0c58674d316052b603f696d577808d
Joyce Ku <joyceku1020@gmail.com>
2026-01-21 15:38:33 -0500

    increased padding and decreased font size on About page

--- src/components/about/Quote/Quote.jsx
+++ src/components/about/Quote/Quote.jsx
@@ -5,8 +5,9 @@ export default function Quote() {
     <section className="about-quote">
       <div className="about-quote__content">
         <p className="about-quote__text">
-          The purpose of Living Grace Ministry is to nurture{" "}
-          <span className="about-quote__emphasis">genuine</span> Christian
+          The purpose of Living Grace<br/>
+          Ministry is to nurture{" "}<br/>
+          <span className="about-quote__emphasis">genuine</span> Christian<br/>
           discipleship through <span className="about-quote__grace">grace</span>.
         </p>
         <div className="about-quote__divider" />
