551aa97499e4567da8acb1cf4184fa79af8f2dc9
Joyce Ku <joyceku1020@gmail.com>
2026-02-04 15:59:47 -0500

    Made new Overview for all ministries page

--- src/components/ministries/Overview/Overview.jsx
+++ src/components/ministries/Overview/Overview.jsx
@@ -2,6 +2,8 @@
 
 import "./Overview.css";
 
+import image from "@/assets/ministry_campus.webp";
+
 export default function Overview({
     subtitle,
     description,
@@ -11,14 +13,20 @@ export default function Overview({
         <section className="overview">
             <div className="overview__container">
                 <div className="overview__left">
-                    <p className="overview__subtitle">{subtitle}</p>
-                    <p className="overview__desc">{description}</p>
-                </div>
-                <div className="overview__right">
-                    <p className="overview__mission-text">{mission}</p>
+                    <p className="overview__subtitle eyebrow">{subtitle}</p>
+                    <p className="overview__desc body-text">{description}</p>
+                    <p className="overview__mission-text section-h3">{mission}</p>
                     <a href="/contact" className="overview__contact-button">
                         Contact Us
                     </a>
+                </div>
+                <div className="overview__right">
+                    <img
+                        className="overview__image"
+                        src={image}
+                        alt=""
+                        loading="lazy"
+                    />
                 </div>
             </div>
         </section>
