a29809942899f39c2c888f58935773a3242605c7
Joel Kim <joelps@umich.edu>
2026-02-12 22:45:24 -0500

    who we are casing + about us hero fix

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import Hero from '@/components/about/Hero/Hero'
 import Quote from '@/components/about/Quote/Quote'
 import Overview from '@/components/about/Overview/Overview'
 import WhoWeAre from '@/components/about/WhoWeAre/WhoWeAre'
+import Hero from "@/components/ministries/Hero/Hero";
+import about_hero from "@/assets/about_hero.webp";
+
 
 export default function About() {
   return (
     <>
-      <Hero />
+      <Hero title="ABOUT US" image={about_hero} />
       <Quote />
       <Overview />
       <WhoWeAre />
