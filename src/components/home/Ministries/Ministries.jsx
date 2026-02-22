863c61e44f61468d963125b6dc8d6eba927d8c36
Tae Hoon Kim <thkimsea@gmail.com>
2026-02-21 23:09:49 -0500

    adjust ministry card photos

--- src/components/home/Ministries/Ministries.jsx
+++ src/components/home/Ministries/Ministries.jsx
@@ -2,19 +2,24 @@ import "./Ministries.css";
 import Card from "@/components/ui/Card/Card";
 import CardCarousel from "@/components/ui/CardCarousel/CardCarousel";
 
-import campusImg from "@/assets/ministry_campus.webp";
-import postgradImg from "@/assets/ministry_postgrad.webp";
-import postgradImg2 from "@/assets/postgradimg.webp";
 import children from "@/assets/children.webp";
 import youthgroup from "@/assets/youth-ministry.webp";
+import campusImg from "@/assets/ministry_campus.webp";
+import postgradImg from "@/assets/ministry_postgrad.webp";
+import adultFamilyImg from "@/assets/ministry_adult_family.webp";
 
 export default function Ministries() {
   const ministries = [
     { id: 1, title: "CHILDREN'S", image: children, href: "/childrens" },
     { id: 2, title: "YOUTH GROUP", image: youthgroup, href: "/youth-group" },
     { id: 3, title: "CAMPUS", image: campusImg, href: "/campus" },
-    { id: 4, title: "POST GRAD", image: postgradImg2, href: "/post-grad" },
-    { id: 5, title: "ADULT/FAMILY", image: postgradImg, href: "/adult-family" },
+    { id: 4, title: "POST GRAD", image: postgradImg, href: "/post-grad" },
+    {
+      id: 5,
+      title: "ADULT/FAMILY",
+      image: adultFamilyImg,
+      href: "/adult-family",
+    },
   ];
 
   return (
